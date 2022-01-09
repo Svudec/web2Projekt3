2a1db85f7656fb161a8d171c93232feedeb5a423
Karlo Sudec <Svudec@users.noreply.github.com>
2022-01-09 15:06:12 +0100

    fixed router

--- src/router/index.js
+++ src/router/index.js
@@ -1,18 +1,23 @@
-import { createRouter, createWebHistory } from "vuerouter";
+import { createRouter, createWebHistory } from "vue-router";
 import Test from '../views/test.vue';
-
+import NotFound from '../views/NotFound.vue'
 
 const routes = [
-  {
-    path: "/",
-    name: "Home",
-    component: Test,
-  },
+    {
+        path: "/",
+        name: "Home",
+        component: Test,
+    },
+    {
+        path: "/:catchAll(.*)",
+        name: "NotFound",
+        component: NotFound,
+    }
 ];
 
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes,
+    history: createWebHistory(process.env.BASE_URL),
+    routes,
 });
 export default router;
