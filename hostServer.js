cd6ba2f8eae52a255a251df54af469caaf811bda
Karlo Sudec <Svudec@users.noreply.github.com>
2022-01-10 01:50:51 +0100

    probao fix indexa

--- hostServer.js
+++ hostServer.js
@@ -4,6 +4,6 @@ const history = require('connect-history-api-fallback');
 const path = require('path');
 let app = express();
 app.use(serveStatic(path.join(__dirname, 'dist')));
-app.use(history());
+app.use(history({index: './public/index.html'}));
 const port = process.env.PORT || 3000;
 app.listen(port);
