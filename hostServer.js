e7d5b37b94750cef928ecc5a8bc3b5b4f5a434b3
Karlo Sudec <Svudec@users.noreply.github.com>
2022-01-10 01:32:42 +0100

    vrati nazad

--- hostServer.js
+++ hostServer.js
@@ -3,6 +3,5 @@ const serveStatic = require("serve-static")
 const path = require('path');
 let app = express();
 app.use(serveStatic(path.join(__dirname, 'dist')));
-app.use(express.static(__dirname + '/public'));
 const port = process.env.PORT || 3000;
 app.listen(port);
