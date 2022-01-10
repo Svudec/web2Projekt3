a3aa36ea02e36f6201ce3ce53ec75aecebcda1a3
Karlo Sudec <Svudec@users.noreply.github.com>
2022-01-10 01:58:14 +0100

    fixxx

--- hostServer.js
+++ hostServer.js
@@ -3,7 +3,7 @@ const serveStatic = require("serve-static")
 const history = require('connect-history-api-fallback');
 const path = require('path');
 let app = express();
+app.use(history());
 app.use(serveStatic(path.join(__dirname, 'dist')));
-app.use(history({index: './public/index.html'}));
 const port = process.env.PORT || 3000;
 app.listen(port);
