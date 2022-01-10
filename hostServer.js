64adddb149de831695413d27abd423a5f01e3512
Karlo Sudec <Svudec@users.noreply.github.com>
2022-01-10 01:44:44 +0100

    Pokušaj popravka routera 2

--- hostServer.js
+++ hostServer.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const serveStatic = require("serve-static")
+const history = require('connect-history-api-fallback');
 const path = require('path');
 let app = express();
 app.use(serveStatic(path.join(__dirname, 'dist')));
+app.use(history());
 const port = process.env.PORT || 3000;
 app.listen(port);
